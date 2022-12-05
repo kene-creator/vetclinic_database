fd9b5e84db1175df49d464ab16cb2c38390707a9
Kene <kayyungx@gmail.com>
2022-12-05 16:45:48 +0100

    Add queries

--- data.sql
+++ data.sql
@@ -2,3 +2,4 @@ INSERT INTO animals(name,date_of_birth,weight_kg,neutered,escape_attempts) VALUE
 INSERT INTO animals(name,date_of_birth,weight_kg,neutered,escape_attempts) VALUES('Gabumon','2018-11-15',8,'true',2);
 INSERT INTO animals(name,date_of_birth,weight_kg,neutered,escape_attempts) VALUES('Pikachu','2021-01-7',15.04,'false',1);
 INSERT INTO animals(name,date_of_birth,weight_kg,neutered,escape_attempts) VALUES('Devimon','2017-05-12',11,'true',5);
+
